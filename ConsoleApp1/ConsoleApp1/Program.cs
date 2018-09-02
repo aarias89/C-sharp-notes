28e732d8928082e6e5f41fa41571017d4bb4b4a4
Andres Arias <andresar11@gmail.com>
2018-09-01 22:24:33 -0400

    Easy class declaration, class object instantiation. Just like you Ruby, you want to keep your classes on separate files.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,11 +2,27 @@
 
 namespace ConsoleApp1
 {
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName );
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-           
+            //Person Frodo = new Person();
+            var Frodo = new Person();
+            Frodo.FirstName = "Frodo";
+            Frodo.LastName = "Baggins";
+            Frodo.Introduce();
+
         }
     }
 }
