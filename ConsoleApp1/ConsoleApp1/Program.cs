454195f7765fbc65c03181992b63b3563665b2a7
Andres Arias <andresar11@gmail.com>
2018-09-02 22:53:31 -0400

    Conditional Statements if/else switch/case

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,53 +3,66 @@
 
 namespace ConsoleApp1
 {
-    public class Person
-    {
-        public int Age;
-    }
+
 
 
     class Program
     {
         static void Main(string[] args)
         {
-            //value types. On the stackm these two variables are completly independent from each other.
-            var a = 10;
-            var b = a;
-            b++;
-            Console.WriteLine(string.Format("a:{0}, b:{1}",a,b));
+            //int hour = 10;
 
-            //reference types.Arrays are a class. so its a reference type
-            //when we initalize the array, the object is created on the heap with a specific memory location, not the stack.
-            //when we make changes to array1 or array2, the value is changed in the heap and affects the values of both arrays on the stack.
-            var array1 = new int[3] { 1, 2, 3, };
-            var array2 = array1;
-            array2[0] = 0;
-            Console.WriteLine(string.Format("array1[0]:{0}, array2[0]: {1}",array1[0],array2[0]));
-
-
-            var number = 1;
-            Increment(number);
-            //number is declared in 2 different scopes, so it has 2 different locations in memory.
-            Console.WriteLine(number);
+            //if (hour > 0 && hour <12)
+            //{
+            //    Console.WriteLine("it's morning");
+            //}
+            //else if (hour >= 12 && hour < 18)
+            //{
+            //    Console.WriteLine("It's afternoon");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It's evening");
+            //}
 
 
-            var person = new Person() { Age = 20 };
-            //the reference from the Makeold person will be copied. So both person objects in main method and MakeOld method reference the same object on the heap.
-            MakeOld(person);
-            Console.WriteLine(person.Age);
+            //bool isGoldCustomer = true;
 
-        }
-        //return type for both of these methods is void, which means they dont return any values.
-        //methods defined as static so there they can be called without the need to create an object.
-        public static void Increment(int number)
-        {
-            number += 10;
-        }
+            //float price;
+            //if (isGoldCustomer)
+            //    price = 19.95f;
+            //else
+            //    price = 29.95f;
 
-        public static void MakeOld(Person person)
-        {
-            person.Age += 10;
+            //float price = (isGoldCustomer) ? 19.95f : 29.95f;
+            //Console.WriteLine(price);
+
+
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+
+                //having these 2 cases stacked like this (Autumn and Summer) will make them share the same block of code("I love this season")
+                //case Season.Autumn:
+                //case Season.Summer:
+                //    Console.WriteLine("I love this season!");
+                //    break;
+                case Season.Autumn:
+                    Console.WriteLine("It's Autumn and a beautiful season");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("It's Summer and its a hot season");
+                    break;
+                case Season.Spring:
+                    Console.WriteLine("It's Spring and its a season full of pollen");
+                    break;
+                default:
+                    Console.WriteLine("This season is too cold to even mention");
+                    break;
+            }
+      
+
         }
     }
 }
---
+++ ConsoleApp1/ConsoleApp1/Season2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public enum Season
+    {
+        //no need to assign numerical values here because we are not working with a database or external sources.
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+}
