ebf8bd037759fd55d8489d239022fb79587bc482
Andres Arias <andresar11@gmail.com>
2018-09-03 19:06:17 -0400

    Using Lists instead of Arrays which are static in size.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
 namespace ConsoleApp1
@@ -8,44 +9,43 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+            var numbers = new List<int>() { 1,12,2,3,4 };
+            numbers.Add(9);
+            numbers.AddRange(new int[3] { 11,12,13 });
 
-            //Length
-            //Indexof 
-            var index = Array.IndexOf(numbers, 9);
-            Console.WriteLine("Index of 9 is : " + index);
-
-            //Clear
-            Array.Clear(numbers, 3, 2);
-            Console.WriteLine("Effects of Clear()");
-            foreach (var n in numbers)
+            foreach (var number in numbers)
             {
-                Console.WriteLine(n);
+                Console.WriteLine(number);
             }
 
+            //IndexOf
 
-            //Copy
-            int[] another = new int[3];
-            Array.Copy(numbers,another,3);
-            Console.WriteLine("Effects of Copy() : ");
-            foreach (int i in another)
-            { Console.WriteLine(i); }
+            Console.WriteLine("index of 12 : " + numbers.IndexOf(12)); 
+            Console.WriteLine("last index of 12 : " + numbers.LastIndexOf(12));
 
-            //Sort 
-            Array.Sort(numbers);
-            Console.WriteLine("Effects of Sort()");
-            foreach (int i in numbers)
+
+            Console.WriteLine("Count: " + numbers.Count);
+
+
+            //Remove
+
+            numbers.Remove(12);
+            Console.WriteLine("Effecs of Remove()");
+            foreach (var number in numbers)
             {
-                Console.WriteLine(i);
+                //it only removes the first instance of the paramater passed to the Remove method.
+                Console.WriteLine(number);
+            }
+        //to remove all instances...use for loop...foreach loops will throw error
+           for (var i =0; i < numbers.Count; i ++)
+            {
+                if (numbers[i] == 12)
+                    numbers.Remove(numbers[i]);
             }
 
-            //Reverse
-            Array.Reverse(numbers);
-            Console.WriteLine("Effects of Reverse()");
-            foreach (var i in numbers)
-            {
-                Console.WriteLine(i);
-            }
+            //Clear()
+            numbers.Clear();
+           Console.WriteLine(numbers.Count);
 
 
         }
