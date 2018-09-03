062ce601ed289ba68b78e3dfcf6a4cf7ee0a7083
Andres Arias <andresar11@gmail.com>
2018-09-03 01:02:21 -0400

    For Loops

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,38 +4,26 @@
 namespace ConsoleApp1
 {
 
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.Write("Enter a number between 1 to 10: ");
-            //var input = Console.ReadLine();
-            //var number = Convert.ToInt32(input);
-            //if (number >= 1 && number <= 10)
-            //    Console.WriteLine("Valid");
-            //else
-            //    Console.WriteLine("Invalid");
-
-            Console.Write("What is the speed limit? ");
-            var speedLimit = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("What is the speed of this car? ");
-            var carSpeed = Convert.ToInt32(Console.ReadLine());
-
-            if (carSpeed < speedLimit)
-                Console.WriteLine("Ok");
-            else
+          
+            for (var i = 1; i <= 10; i++)
             {
-                const int kmPerDemeritPoint = 5;
-                var demeritPoints = (carSpeed - speedLimit) / kmPerDemeritPoint;
-                if (demeritPoints > 12)
-                    Console.WriteLine("License Suspended");
-                else
-                    Console.WriteLine("Demerit points: " + demeritPoints);
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
             }
 
+            for (var i=10; i >= 1; i--)
+            {
+                if (i%2 ==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
