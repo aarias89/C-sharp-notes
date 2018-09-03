efd51ba7e49f5e974b0a70369378f7ffd2f8c56c
Andres Arias <andresar11@gmail.com>
2018-09-03 18:39:24 -0400

    Working with Arrays and  common array methods

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,23 +8,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //this method is used for creating random numbers
-            //var random = new Random();
-            //for (var i = 0; i < 10; i++)
-            //    Console.WriteLine(random.Next(1,20));
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
 
-            //one can also create random "passwords" or set of characters using ASCII values by casting the values to char type
-            const int passwordLength = 10;
-            var random = new Random();
-            var buffer = new char[passwordLength];
-            for (var i = 0; i < passwordLength; i++)
-                //97 and 122 are ASCII values of a and z
-                //Console.Write((char)random.Next(97,122));
-                buffer[i] = (char)random.Next(97,122);
-            var password = new string(buffer);
-            Console.WriteLine(password);
+            //Length
+            //Indexof 
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9 is : " + index);
+
+            //Clear
+            Array.Clear(numbers, 3, 2);
+            Console.WriteLine("Effects of Clear()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+
+            //Copy
+            int[] another = new int[3];
+            Array.Copy(numbers,another,3);
+            Console.WriteLine("Effects of Copy() : ");
+            foreach (int i in another)
+            { Console.WriteLine(i); }
+
+            //Sort 
+            Array.Sort(numbers);
+            Console.WriteLine("Effects of Sort()");
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Reverse
+            Array.Reverse(numbers);
+            Console.WriteLine("Effects of Reverse()");
+            foreach (var i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+
         }
     }
 }
 
-//strings are immutable in c# so to work around this we can create an array to save the characters or letters...since arrays are mutable.
